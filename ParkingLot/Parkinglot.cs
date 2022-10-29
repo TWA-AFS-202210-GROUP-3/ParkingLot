7a5a58fd4c69521cc64c226675b02fcc13c67322
RZhang16 <RZhang16@slb.com>
2022-10-29 22:38:58 +0800

    feat: Add first test

---
+++ ParkingLot/Parkinglot.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    public class Parkinglot
+    {
+        public IList<string> Ticket { get; set; }
+
+        public bool Remove(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+public class CourseClass
+{
+    public string ClassName { get; }
+    private Teacher classTeacher;
+    private IList<Student> classStudents;
+
+    public CourseClass(string className)
+    {
+        ClassName = className;
+        classStudents = new List<Student>();
+    }
+
+    public bool AddTeacher(Teacher teacher)
+    {
+        if (classTeacher != null)
+        {
+            return false;
+        }
+
+        classTeacher = teacher;
+        teacher.CourseClass = this;
+        return true;
+    }
+
+    public ClassWelcome AddStudent(Student student)
+    {
+        if (classStudents.Any(_ => _.Id == student.Id))
+        {
+            return new ClassWelcome();
+        }
+
+        student.CourseClass = this;
+        var welcome = new ClassWelcome
+        {
+            TeacherWelcome = classTeacher?.Welcome(student) ?? string.Empty,
+            StudentsWelcome = classStudents.Select(classStudent => classStudent.Welcome(student)).ToList(),
+        };
+        classStudents.Add(student);
+        return welcome;
+    }
+}
--- ParkingLotTest/UnitTest1.cs
+++ ParkingLotTest/UnitTest1.cs
@@ -1,15 +1,36 @@
 namespace ParkingLotTest
 {
-    using ParkingLot;
+    using Parking;
     using Xunit;
 
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_return_remove_one_ticket_And_fetch_car_when_get_car()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            var parkingboy = new Parkingboy();
+            var parkinglot = new Parkinglot();
+            var car = new Car();
+
+            var resultremove = parkinglot.Remove(car);
+            var resultfetch = parkingboy.Fetchcar(car);
+
+            Assert.True(resultremove);
+            Assert.True(resultfetch);
+        }
+
+        [Fact]
+        public void Should_return_get_ticket_And_park_car_when_parking_new_car()
+        {
+            var parkingboy = new Parkingboy();
+            var parkinglot = new Parkinglot();
+            var car = new Car();
+
+            var resultadd = parkinglot.Add(car);
+            var resultpark = parkingboy.Parkcar(car);
+
+            Assert.True(resultadd);
+            Assert.True(resultpark);
         }
     }
 }
