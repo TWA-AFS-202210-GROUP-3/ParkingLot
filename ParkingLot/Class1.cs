5e6c92913c930a5447f8757621f89f9ac01c2b9a
RZhang16 <RZhang16@slb.com>
2022-10-30 15:47:50 +0800

    feat: Add warning

---
+++ ParkingLot/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotProject
+{
+    public class NoTicketException : Exception
+    {
+        public NoTicketException(string message) : base(message)
+        {
+        }
+    }
+
+    public class NoPositionException : Exception
+    {
+        public NoPositionException(string message) : base(message)
+        {
+        }
+    }
+
+    public class WrongCarException : Exception
+    {
+    }
+
+    public class WrongTicketException : Exception
+    {
+        public WrongTicketException(string message) : base(message)
+        {
+        }
+    }
+}
