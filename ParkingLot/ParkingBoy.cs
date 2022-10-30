2ad7e4e7138290684b899446ab6902bde3b7bc8a
Qinlan Weng <QWeng@slb.com>
2022-10-30 19:04:27 +0800

    test: add test for AC3 second half

--- ParkingLot/ParkingBoy.cs
+++ ParkingLot/ParkingBoy.cs
@@ -29,6 +29,11 @@ namespace ParkingLot
 
         public bool FetchCar(ParkingTicket parkingTicket, ParkingLot parkingLot)
         {
+            if (parkingTicket == null)
+            {
+                return false;
+            }
+
             return parkingLot.RemoveCar(parkingTicket.CarName);
         }
     }
